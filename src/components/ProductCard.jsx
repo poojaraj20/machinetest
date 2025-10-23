436ec223dfc475312e34d15cfe225ed81742c54b
Pooja Raj <pooja@example.com>
2025-10-23 10:27:40 +0530

    updated commit

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
       <div className="card h-100">
         <img src={product.image} className="card-img-top" alt={product.title} style={{ height: '200px', objectFit: 'contain' }} />
         <div className="card-body">
-          <h5 className="card-title">{product.title}</h5>
+          <h5 className="card-title text-center">{product.title}</h5>
           <p className="card-text text-center">${product.price}</p>
           <div className="text-center">
             <Link to={`/product/${product.id}`} className="btn btn-info text-center">
